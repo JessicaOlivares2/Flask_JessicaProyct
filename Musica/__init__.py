7db33551ee734dc56d2b60b8639953397a74a464
Jessica Olivares <jessicaolivares863@gmail.com>
2024-05-06 10:48:04 -0300

    hice el template y la ruta de generos

--- Musica/__init__.py
+++ Musica/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,3 +10,14 @@ with app.app_context() :
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/generos')
+def generos():
+    base_de_datos = db.get_db()
+    consulta = """
+        SELECT name FROM genres
+        ORDER by name;
+    """
+    resultado = base_de_datos.execute(consulta)
+    lista_de_resultado = resultado.fetchall()
+    return render_template("generos.html",generos=lista_de_resultado)
